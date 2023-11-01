e5a4dc0164d5ac6124d1896c1db75310d2bf48f7
muhammad.bilal1 <muhammad.bilal1@axabiztech.com>
2023-11-01 11:02:44 +0500

    revert changes

--- app/src/main/java/com/simbaone/lifehacks/data/models/CategoryDTO.kt
+++ app/src/main/java/com/simbaone/lifehacks/data/models/CategoryDTO.kt
@@ -6,11 +6,11 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CategoryDTO(
-    @SerializedName("id") val id: String? = "",
-    @SerializedName("name") val name: String? = "",
-    @SerializedName("fileName") val fileName: String? = "",
-    @SerializedName("iconName") val iconName: String? = "",
-    @SerializedName("count") val count: Int? = 100,
+    @SerializedName("id") val id: String = "",
+    @SerializedName("name") val name: String = "",
+    @SerializedName("fileName") val fileName: String = "",
+    @SerializedName("iconName") val iconName: String = "",
+    @SerializedName("count") val count: Int = 100,
     @SerializedName("credit") val credit: String? = null,
     @SerializedName("color") val bgColor: Int? = null,
 ): Parcelable
